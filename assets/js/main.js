bde79f38e61c829e9757811e89f0a67a646a271b
arajali123 <arajali.sds@gmail.com>
2026-03-05 16:23:10 +0600

    collection section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,29 @@
 
+// Collection Js
+var flkty = new Flickity('#burger-carousel', {
+  cellAlign: 'left',
+  contain: true,
+  wrapAround: true,
+  pageDots: false,
+  prevNextButtons:false
+});
+
+// Progress bar
+var progressBar = document.querySelector('.collection-progress-bar');
+
+flkty.on('scroll', function(progress) {
+  progress = Math.max(0, Math.min(1, progress));
+  progressBar.style.width = (progress * 100) + '%';
+});
+
+// Arrows
+document.querySelector('.flickity-prev').addEventListener('click', function() {
+  flkty.previous();
+});
+
+document.querySelector('.flickity-next').addEventListener('click', function() {
+  flkty.next();
+});
 
 
 
